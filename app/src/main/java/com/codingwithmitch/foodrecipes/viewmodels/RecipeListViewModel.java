8a5d0a9571a6630fe564682ff34ed01837a77518
Fahri MacBook <fahri-can@web.de>
2020-05-09 20:07:17 +0200

    Add an Enum and LiveData for ViewState.

--- app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeListViewModel.java
@@ -3,18 +3,32 @@ package com.codingwithmitch.foodrecipes.viewmodels;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MediatorLiveData;
 import android.support.annotation.NonNull;
 
 public class RecipeListViewModel extends AndroidViewModel {
 
     private static final String TAG = "RecipeListViewModel";
 
+    public enum ViewState {CATEGORIES, RECIPES}
+
+    private MediatorLiveData<ViewState> viewState;
+
     public RecipeListViewModel(@NonNull Application application) {
         super(application);
-
+        init();
     }
 
+    private void init() {
+        if (viewState == null) {
+            viewState = new MediatorLiveData<>();
+            viewState.setValue(ViewState.CATEGORIES);
+        }
+    }
 
+    public MediatorLiveData<ViewState> getViewState() {
+        return viewState;
+    }
 }
 
 
